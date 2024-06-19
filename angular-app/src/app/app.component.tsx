a1d360e40c17484269a4311fdd7925f84b128efc
HariharanIT <ghariharanit@gmail.com>
2024-06-19 16:51:15 +0530

    Updated the qa testing

--- angular-app/src/app/app.component.tsx
+++ angular-app/src/app/app.component.tsx
@@ -1,8 +1,5 @@
 import { Component } from "@angular/core";
-import AppBuilderWebSdk, { React } from "@appbuilder/web";
-import DummyComponent, { useAfterEndCallHook } from "./DummyComponent";
-import WrapperProvider from "./app.wrapper";
-console.log("debugging AppBuilderWebSDK", AppBuilderWebSdk);
+import AppBuilderWebSdk from "@appbuilder/web";
 @Component({
   selector: "app-root",
   template: `
@@ -18,43 +15,8 @@ export class AppComponent {
   title = "angular-app";
 
   ngOnInit() {
-    const myCustomization = AppBuilderWebSdk.createCustomization({
-      components: {
-        videoCall: {
-          wrapper: WrapperProvider,
-        },
-      },
-      config: {
-        defaultRootFallback: DummyComponent,
-      },
-      lifecycle: {
-        useAfterEndCall: useAfterEndCallHook,
-      },
-    });
+    const myCustomization = AppBuilderWebSdk.createCustomization({});
     AppBuilderWebSdk.customize(myCustomization);
-
-    //token
-    const token =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoZW50aWNhdGlvbl90eXBlIjoxLCJhcHBfaWQiOiI5YWM0YzhmMThkMjU0YTc0OWNkYzkzYTlhMTc0Njc4OSIsInVzZXJfaWQiOiJjLWZlZmFhZWRjLWViM2EtNGRmOS1hZThiLTgwMTA2MzY2YzBlOCIsInByb2plY3RfaWQiOiI2NjRmNzVkZmY4YTU0M2RlNzI1NCIsImV4cCI6MTcxODI2MzM0Nn0.u4zafqTALg3CR4VObNrkUesFf55H365zeZEF07Xig7c";
-    AppBuilderWebSdk.login(token)
-      .then(() => {
-        console.log("debugging login success");
-        //host
-        AppBuilderWebSdk.joinPrecall(
-          "3bfe2c35-dd75-4a9b-a198-6982ac7da96f",
-          "",
-          false
-        );
-        // //attendee
-        // AppBuilderWebSdk.joinPrecall(
-        //   "e100eb1c-b8ca-4d4c-9060-850d591cd1a7",
-        //   "",
-        //   false
-        // );
-      })
-      .catch((err) => {
-        console.log("debugging login failure", err);
-      });
   }
 
   ngOnDestroy() {}
